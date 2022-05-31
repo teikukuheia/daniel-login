6863fe7989ce3dfc3698e361832b0f4c87a2bc98
teikukuheia <teikukuheia@gmail.com>
2022-05-31 21:35:50 +0300

    dark theme and idk

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-const button = document.querySelectorAll(".button");
-const frame = document.querySelector(".background");
+// const button = document.querySelectorAll(".button");
+// const frame = document.querySelector(".background");
 
-button.forEach(button => {
-    button.addEventListener("click", function(){
-        this.innerHTML = "bai bai nigger, back to the lobby";
-        setInterval(function(){
-            location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-        }, 1500);
-    });
+// button.forEach(button => {
+//     button.addEventListener("click", function(){
+//         this.innerHTML = "bai bai nigger, back to the lobby";
+//         setInterval(function(){
+//             location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+//         }, 1500);
+//     });
+// });
+
+const dark_theme_button = document.querySelector(".dark-theme-button");
+const light_theme_button = document.querySelector(".light-theme-button");
+const body = document.body;
+
+$(".light-theme-button").toggle();
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+     $(".dark-theme-button").toggle();
+     $(".light-theme-button").toggle();
+     body.classList.toggle("dark-mode");
+}
+
+dark_theme_button.addEventListener("click", function(){
+     $(".dark-theme-button").toggle();
+     $(".light-theme-button").toggle();
+     body.classList.toggle("dark-mode");
+});
+
+light_theme_button.addEventListener("click", function(){
+     $(".light-theme-button").toggle();
+     $(".dark-theme-button").toggle();
+     body.classList.toggle("dark-mode");
 });
